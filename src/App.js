d3ffe17d98220abfd33091e576fc8323a8eed53d
thomasgit123 <thomasbox123@gmail.com>
2019-12-17 09:55:48 -0500

    auto commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Router, Switch, Route } from 'react-router-dom'
-import * as history from 'history'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Header from './pages/Header'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -8,7 +7,7 @@ import About from './pages/About'
 function App() {
   return (
     <div className="flex flex-col h-full items-center justify-center bg-blue-dark text-white">
-      <Router basename={process.env.PUBLIC_URL} history={history.createBrowserHistory()}>
+      <Router basename={process.env.PUBLIC_URL}>
         <Header />
         <Switch>
           <Route exact path="/">
